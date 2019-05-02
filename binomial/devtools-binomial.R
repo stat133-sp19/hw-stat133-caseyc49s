3c9e7f85a9f9a128e63c2335e928f506ee1e4522
caseyc49s <casey.chadwell@berkeley.edu>
2019-05-02 16:22:36 -0700

    final commit, updated vignette

--- binomial/devtools-binomial.R
+++ binomial/devtools-binomial.R
@@ -2,10 +2,11 @@
 # Devtools workflow
 # =====================================================
 
+# This file is not original and was copied from the PackYourCode book by Gaston Sanchez
+
 devtools::document()          # generate documentation
 devtools::check_man()         # check documentation
 devtools::test()              # run tests
 devtools::build_vignettes()   # build vignettes
 devtools::build()             # build bundle
 devtools::install()           # install package
-devtools::check()             # comprehensive check (optional)
